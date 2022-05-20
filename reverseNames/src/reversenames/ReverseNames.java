eccffe4795226d492dbf34c004f2799e877a3073
Kevin Bell <bellKevin@pm.me>
2022-05-20 16:00:39 +0000

    i have little to no idea what i'm doing

--- reverseNames/src/reversenames/ReverseNames.java
+++ reverseNames/src/reversenames/ReverseNames.java
@@ -8,18 +8,16 @@ public class ReverseNames {
     public static void main(String[] args) {
         System.out.print("Reverse Name by Kevin Bell \n");
         Scanner computerKeyboardInput = new Scanner(System.in);
-        System.out.println("Enter your first name: ");
+        System.out.print("Enter your first name: ");
         String firstName = computerKeyboardInput.next();
-        System.out.println("Enter your middle name: ");
-        String middle = computerKeyboardInput.next();
-        System.out.println("Enter your last name: ");
-        String last = computerKeyboardInput.next();
-        String name = firstName + " " + middle + " " + last;
+        String name = firstName;
         for (int i = name.length() - 1; i >= 0; i--) {
             System.out.print(name.charAt(i));
         }
         System.out.println("");
-        String words[] = name.split("\\s");
+        String str = name;
+        //Split sentence into words
+        String words[] = str.split("\\s");
         String newString = "";
         for (String w : words) {
             String first = w.substring(0, 1);      //First Letter
@@ -31,5 +29,7 @@ public class ReverseNames {
         //trim to remove the last redundant blank space
         System.out.println(newString.trim());
         System.out.println("");
+        //System.out.println(firstName + " "+ last);
+        //System.out.println("");
     }
 }
