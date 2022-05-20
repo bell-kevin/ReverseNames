527fc5152661244905ad6f35d5eba1d6c6f21c57
Kevin Bell <bellKevin@pm.me>
2022-05-20 19:10:11 +0000

    got it!

--- reverseNames/src/reversenames/ReverseNames.java
+++ reverseNames/src/reversenames/ReverseNames.java
@@ -5,23 +5,24 @@ import java.util.Scanner;
 public class ReverseNames {
 
     public static void main(String[] args) {
-        String firstName, lastName, reversedFirstname = "", reversedLastName="";
+        String firstName, lastName, reversedFirstname = "", reversedLastName = "";
         System.out.print("Reverse Name by Kevin Bell \n");
         Scanner computerKeyboardInput = new Scanner(System.in);
         System.out.print("What is your name? ");
-        firstName = computerKeyboardInput.next();
-        lastName = computerKeyboardInput.next();
+        firstName = computerKeyboardInput.next().toLowerCase();
+        lastName = computerKeyboardInput.next().toLowerCase();
         for (int i = firstName.length() - 1; i >= 0; i--) {
             reversedFirstname = reversedFirstname + firstName.charAt(i);
+            //if (reversedFirstname.length() == 1) {
+                //reversedFirstname = reversedFirstname.toUpperCase();
+            //}
         }
-        //capitalize first letter of first name
-        reversedFirstname = reversedFirstname.toUpperCase(0);
+        reversedFirstname = reversedFirstname.substring(0, 1).toUpperCase() + reversedFirstname.substring(1);
         System.out.print(reversedFirstname + " ");
         for (int i = lastName.length() - 1; i >= 0; i--) {
             reversedLastName = reversedLastName + lastName.charAt(i);
         }
-        //capitalize first letter of last name
-        reversedLastName = reversedLastName.toUpperCase(0);
+        reversedLastName = reversedLastName.substring(0, 1).toUpperCase() + reversedLastName.substring(1);
         System.out.println(reversedLastName);
     }
 }
