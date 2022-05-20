4e73274678569843ba4629964cb1ed083d948e41
Kevin Bell <bellKevin@pm.me>
2022-05-20 18:40:24 +0000

    new plan getting better

--- reverseNames/src/reversenames/ReverseNames.java
+++ reverseNames/src/reversenames/ReverseNames.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class ReverseNames {
 
     public static void main(String[] args) {
-        String firstName, lastName, reversedFirstname, reversedLastName;
+        String firstName, lastName, reversedFirstname = "", reversedLastName="";
         System.out.print("Reverse Name by Kevin Bell \n");
         Scanner computerKeyboardInput = new Scanner(System.in);
         System.out.print("What is your name? ");
         firstName = computerKeyboardInput.next();
         lastName = computerKeyboardInput.next();
         for (int i = firstName.length() - 1; i >= 0; i--) {
-            reversedFirstname += firstName.charAt(i);
+            reversedFirstname = reversedFirstname + firstName.charAt(i);
         }
+        System.out.print(reversedFirstname + " ");
+        for (int i = lastName.length() - 1; i >= 0; i--) {
+            reversedLastName = reversedLastName + lastName.charAt(i);
+        }
+        System.out.println(reversedLastName);
     }
 }
